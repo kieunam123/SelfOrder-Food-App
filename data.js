3851002e56e9119b81da00afe6232622618b0f95
kieunam123 <kieunam011@gmail.com>
2022-04-01 22:36:01 +0700

    updated by me

--- data.js
+++ data.js
@@ -1,12 +1,12 @@
 const data = {
     categories: [
         {
-            name:'Beverages',
+            name:'Đồ Uống',
             image:'/images/beverages.jpg',
 
         },
         {
-            name : 'Breakfast',
+            name : 'Bữa Sáng',
             image: '/images/breakfast.jpg',
         },
         {
@@ -14,6 +14,89 @@ const data = {
             image: '/images/burgers.jpg',
         },
     ],
+    products:[
+        {
+            category: 'Đồ Uống',
+            name: 'Coca',
+            khuyenmai: 'mua 1 tặng 1',
+            price: 12000,
+            image: '/images/coca.jpg',
+            
+        },
+        {
+            category: 'Đồ Uống',
+            name: 'Sôcôla sữa Shake',
+            khuyenmai: 'giảm 5%',
+            price: 25000,
+            image: '/images/socola.jpg',
+        },
+        {
+            category: 'Đồ Uống',
+            name: 'Bạc xỉu',
+            khuyenmai: 'Freeship',
+            price: 10000,
+            image: '/images/bacxiu.jpg',
+        },
+        {
+            category: 'Bữa Sáng',
+            name: 'Bánh mì',
+            khuyenmai: 'Freeship',
+            price: 20000,
+            image: '/images/banh-mi.png',
+        },
+        {
+            category: 'Bữa Sáng',
+            name: 'Cơm Sườn',
+            khuyenmai: 'Freeship',
+            price: 30000,
+            image: '/images/com-suon.jpg',
+        },
+        {            
+            category: 'Bữa Sáng',
+            name: 'Bún Real',
+            khuyenmai: 'Freeship',
+            price: 30000,
+            image: '/images/bun-real-xuong.jpg',
+        },
+        {
+            category: 'Bữa Sáng',
+            name: 'Phở Bò',
+            khuyenmai: 'Freeship',
+            price: 30000,
+            image: '/images/pho.jpeg',
+        },
+        {
+            category: 'Burgers',
+            name: 'Burger thường',
+            khuyenmai: 'Freeship',
+            price: 25000,
+            image: '/images/burger-thuong.jpg',
+        },
+        {
+            category: 'Burgers',
+            name: 'Burger đặc biệt',
+            khuyenmai: 'Freeship',
+            price: 30000,
+            image: '/images/burger-2-tang.jpg',
+        },
+        {
+            category: 'Burgers',
+            name: 'Burger siêu to khổng lồ',
+            khuyenmai: 'Freeship',
+            price: 35000,
+            image: '/images/burger-dac-biet.jpg',
+        },
+        {
+            category: 'Burgers',
+            name: 'Sandwitch',
+            khuyenmai: 'Freeship',
+            price: 25000,
+            image: '/images/sandwitch.jpg',
+        }
+
+
+
+    ]
 };
 
 module.exports = data;
--- server.js
+++ server.js
@@ -1,7 +1,33 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 const data = require('./data');
 
 const app = express();
+dotenv.config();
+
+mongoose.connect(process.env.MONGODB_URI,{
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+});
+
+const Product = mongoose.model(
+    'product',
+    new mongoose.Schema({
+    name: String,
+    description: String,
+    image: String,
+    price: Number,
+    khuyenmai: String,
+    category: String,
+}));
+
+app.get('/api/products/seed',async(req,res)=>{
+    const products = await Product.insertMany(data.products);
+    res.send({products});
+})
+
 app.get('/api/categories', (req, res)=> {
     res.send(data.categories);
 });
